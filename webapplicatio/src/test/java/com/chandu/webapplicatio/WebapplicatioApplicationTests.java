2f724471bef3bb90c3b600278807ff9897b6284a
chand9346 <cheerlachandrasekhar8@gmail.com>
2024-11-28 08:36:42 +0530

    Add files via upload

---
+++ webapplicatio/src/test/java/com/chandu/webapplicatio/WebapplicatioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandu.webapplicatio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapplicatioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ webapplicatio/webapplicatio/src/main/java/com/chandu/webapplicatio/WebapplicatioApplication.java
@@ -0,0 +1,13 @@
+package com.chandu.webapplicatio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebapplicatioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebapplicatioApplication.class, args);
+	}
+
+}
